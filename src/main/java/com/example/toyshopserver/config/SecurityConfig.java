f2b53e591586d84eaceea0f10eaae03191557cb6
Pawel Matyszczak <pawel.matyszczak@bluesoft.com>
2023-05-29 20:55:01 +0200

    cors config

--- src/main/java/com/example/toyshopserver/config/SecurityConfig.java
+++ src/main/java/com/example/toyshopserver/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -35,8 +36,7 @@ public class SecurityConfig {
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
-        .and()
+    http.cors(AbstractHttpConfigurer::disable)
         .csrf().disable()
         .headers().frameOptions().disable()
         .and()
